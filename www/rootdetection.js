59743503dd045af9625488951f7eba61fde0eec6
langxiankui <33216671+langxiankui@users.noreply.github.com>
2018-11-14 13:44:19 +0800

    Update rootdetection.js
    添加模拟器识别功能，目前仅能识别Android SDK中的模拟器

--- www/rootdetection.js
+++ www/rootdetection.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.isDeviceRooted = function(success, error) {
     exec(success, error, "RootDetection", "isDeviceRooted", []);
 };
+
+exports.isEmulate = function (success, error) {
+    exec(success, error, "RootDetection", "isEmulate", []);
+}
